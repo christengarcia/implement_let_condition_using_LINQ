b0fe0801cfb2991c78491ab9d29f1c6ace87e7ee
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-25 21:56:57 +1100

    Initial commit

---
+++ implement_let_condition_using_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  C# Program to Implement Let Condition using LINQ
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace implement_let_condition_using_LINQ
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Regno { get; set; }
+        public int Marks { get; set; }
+
+    }
+}
